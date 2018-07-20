6cefa42e460024e19b629fb8f9567a85915ae588
Jakub Lewandowski <kubik92@gmail.com>
2018-07-20 16:01:33 +0200

    dodawanie generacji konstruktora

--- src/main/java/co/masterlabs/getGenerator/App.java
+++ src/main/java/co/masterlabs/getGenerator/App.java
@@ -1,19 +1,20 @@
 package co.masterlabs.getGenerator;
 import java.io.File;
-import java.util.List;
+import java.util.Map;
 
 import javax.naming.directory.InvalidAttributesException;
 
 
 /*
-    Program służący do generowania setterów i getterów poza środowiskiem Domino
+    Program służący do generowania setterów, getterów i domyślnego konstruktora pobierającego dane z NotesDocument poza środowiskiem Domino
     dane wejściowe do programu:
         ścieżka do pliku .java / .txt, w którym znajduje się lista pól w formacie:
+        public class MojaKlasa
         {
             int pole1;
             List<Object> lista_czegoś : ileTegoCzegoś;
             itd
-        }}
+        }
 
     dane wyjściowe:
         wygenerowany w dokumentach użytkownika plik .txt z getterami i setterami; dla list generuje gettery w postaci getLista_czegoś1, getLista_czegoś2 itd
@@ -31,7 +32,8 @@ public class App
     { 
         try{
 
-            List<String> list = JavaClassParser.parseClassFile(new File(args[0]));
+            Map<String,Object> map = JavaClassParser.parseClassFile(new File(args[0]));
+            FileGenerator.generateFile(map, args[0]);
         }
         catch (InvalidAttributesException e){
             e.printStackTrace();
